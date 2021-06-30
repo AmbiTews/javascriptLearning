f4de755c397f00c6ad029639f47189b7d37c363e
AmbiTews <36221861+AmbiTews@users.noreply.github.com>
2021-06-30 14:12:42 +0530

    Update assignment.js

--- js/assignment.js
+++ js/assignment.js
@@ -1,6 +1,27 @@
+/*
+//Assignment 1
+
 let country = "India";
 let continent = "Asia";
 let population = "3";
 console.log(country);
 console.log(continent);
 console.log(population);
+
+*/
+
+//Assignment 2 data1
+const markMass = 78;
+const johnMass = 92;
+const markHeight = 1.69;
+const johnHeight = 1.95;
+
+const markHigherBMI;
+const markBMI = markMass / (markHeight ** 2);
+const johnBMI = johnMass / (johnHeight ** 2);
+
+markHigherBMI = markBMI > johnBMI
+console.log("Mark's BMI is Higher then John BMI " +markHigherBMI);
+
+
+
