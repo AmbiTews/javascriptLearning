ce94e0d3de6d199f2d45f37570cde0e966128ea6
AmbiTews <36221861+AmbiTews@users.noreply.github.com>
2021-06-30 14:20:11 +0530

    Update assignment.js

--- js/assignment.js
+++ js/assignment.js
@@ -11,6 +11,7 @@ console.log(population);
 */
 
 //Assignment 2 data1
+/*
 const markMass = 78;
 const johnMass = 92;
 const markHeight = 1.69;
@@ -21,7 +22,25 @@ const markBMI = markMass / (markHeight ** 2);
 const johnBMI = johnMass / (johnHeight ** 2);
 
 markHigherBMI = markBMI > johnBMI
-console.log("Mark's BMI is Higher then John BMI " +markHigherBMI);
+console.log("Mark's BMI is Higher then John BMI " + markHigherBMI);
+*/
+
+
+//Assignment 2 data2
+
+const markMass = 95;
+const johnMass = 85;
+const markHeight = 1.88;
+const johnHeight = 1.76;
+
+let markHigherBMI;
+const markBMI = markMass / (markHeight ** 2);
+const johnBMI = johnMass / (johnHeight ** 2);
+const diffBMI = johnBMI - markBMI;
+markHigherBMI = johnBMI > markBMI
+console.log("Mark's BMI is lower than John's!" + markHigherBMI);
+console.log("Mark's BMI " + markBMI + " is lower  than John's! " + johnBMI + " by " + diffBMI);
+
 
 
 
